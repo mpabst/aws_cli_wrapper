c728ea1904a61dbb29490e1d963b433dc6e842f7
Michael Pabst <michael.k.pabst@gmail.com>
2015-10-27 16:29:44 +0000

    - Fixed to actually work - Can now also do aws.opsworks.describe_stacks

--- lib/aws_cli_wrapper.rb
+++ lib/aws_cli_wrapper.rb
@@ -2,49 +2,60 @@ require 'erb'
 require 'tempfile'
 require 'json'
 
-module AwsCliWrapper
-  def erb(file)
-    path = "#{file}.json.erb"
-    if File.exists?(path)
-      tmp = Tempfile.new(file.gsub('/', '_') + '.json')
-      tmp.write(ERB.new(File.read(path)).result)
-      tmp.close # flush to disk
-      tmp.path
-    end
-  end
+class AwsCliWrapper < Struct.new(:binding)
 
-  def run(_scope, _command, _opts = {})
-    def fmt(s)
+  class Inner < Struct.new(:binding, :service)
+    def initialize(binding, service)
+      super(binding, _format_token(service))
+    end
+
+    def method_missing(meth, *args)
+      _run(meth, *args)
+    end
+
+    def _erb(file)
+      path = "#{file}.json.erb"
+      if File.exists?(path)
+        tmp = Tempfile.new(file.gsub('/', '_') + '.json')
+        tmp.write(ERB.new(File.read(path)).result(binding))
+        tmp.close # flush to disk
+        tmp.path
+      end
+    end
+
+    def _format_token(s)
       s.to_s.gsub(/[ _]/, '-')
     end
 
-    scope, command = *[_scope, _command].map{|p| fmt(p) }
-    opts = _opts.inject({}){|o,(k,v)| o.update(fmt(k) => v) }
+    def _run(_command, _opts = {})
+      command = _format_token(_command)
+      opts = _opts.inject({}){|o,(k,v)| o.update(_format_token(k) => v) }
 
-    if file = opts.delete('json')
-      path = ((tmp = erb(file)) ? tmp : file + '.json')
-      opts['cli-input-json'] = 'file://' + path
-    end
+      if file = opts.delete('json')
+        path = ((tmp = erb(file)) ? tmp : file + '.json')
+        opts['cli-input-json'] = 'file://' + path
+      end
 
-    sh = (
-      ['aws', scope, command] +
-      opts.map{|(k,v)| "--#{k} '#{v}'" }
-    ) * ' '
-    puts sh
+      sh = (
+        ['aws', service, command] +
+        opts.map{|(k,v)| "--#{k} '#{v}'" }
+      ) * ' '
+      puts sh
 
-    out = %x{#{sh}}
-    begin
-      JSON.parse(out)
-    rescue JSON::ParserError
-      out
-    end
-  end
-
-  %w( opsworks ec2 elb rds elasticache s3api cloudfront ).each do |svc|
-    define_method(svc) do |cmd, opts = {}|
-      run(cmd, opts)
+      out = %x{#{sh}}
+      begin
+        JSON.parse(out)
+      rescue JSON::ParserError
+        out
+      end
     end
   end
 
-  extend self
+  %w( opsworks ec2 elb rds elasticache s3api cloudfront ).each do |service|
+    define_method(service) do |command = nil, opts = {}|
+      i = Inner.new(binding, service)
+      command ? i._run(command, opts) : i
+    end
+  end
+
 end
